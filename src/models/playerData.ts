4d2460e2d5f8820f60d2a7176e399f776d135a4d
YoYolops <yohanhxh@gmail.com>
2021-06-03 21:55:35 -0300

    adicionando json de racas

--- src/models/playerData.ts
+++ src/models/playerData.ts
@@ -3,16 +3,25 @@ import mongoose from 'mongoose'
 /* modificadores, perícias, capacidade de carregar peso */
 
 const PlayerDataSchema = new mongoose.Schema({
-    characterAttributes: {
-        forca: Number,
-        destreza: Number,
-        constituicao: Number,
-        inteligencia: Number,
-        sabedoria: Number,
-        carisma: Number
+    characterName: String,
+    password: String,
+    characterData: {
+        characterAttributes: {
+            forca: Number,
+            destreza: Number,
+            constituicao: Number,
+            inteligencia: Number,
+            sabedoria: Number,
+            carisma: Number
+        },
+        raca: String, /* Raça */
+        classe: String,
     },
-    raca: String, /* Raça */
-    classe: String,
+    possessions: {
+        itemIds: Array,
+        spellIds: Array,
+        gold: Number
+    }
 })
 
 export default mongoose.model('PlayerDataSchema')
